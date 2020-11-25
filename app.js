c27f7ee45ca63d36f0c720295a785c28cfba509e
Marcos Porto <39508000+marcosvporto@users.noreply.github.com>
2020-11-25 14:31:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,17 @@ router.get("/", function(req,res){
 router.post("/", function(req,res){
     const { exec } = require('child_process')
 
-    exec('stress --cpu 8 --io 4 --vm 2 --vm-bytes 128M --timeout 10s -v > stresslog.txt', (error, stdout, stderr) => {
+    exec('stress --cpu 8 --io 4 --vm 2 --vm-bytes 128M --timeout 10s -v >> stresslog.txt', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`)
+            return
+        } else {
+            res.sendFile(path.join(__dirname+'/stresslog.txt'))
+        }
+        console.log(`stdout: ${stdout}`)
+        console.log(`stderr: ${stderr}`)
+    })
+    exec('wget -q -O - http://169.254.169.254/meta-data/instance-id >> stresslog.txt', (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`)
             return
